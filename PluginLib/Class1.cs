67a40ab9921be13986081d367e5c40bb620c932d
Savr123 <Sangievv@mail.ru>
2024-03-28 08:55:34 +0300

    Initial commit

---
+++ PluginLib/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System.Drawing;
+
+namespace PluginLib
+{
+    public class Plugins : IPluginFactory
+    {
+        public int PluginsCount => throw new NotImplementedException();
+
+        public string[] GetPluginNames => throw new NotImplementedException();
+
+        public IPlugin GetPlugin(string pluginName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    interface IPluginFactory
+    {
+        int PluginsCount { get; }
+        string[] GetPluginNames { get; }
+        IPlugin GetPlugin(string pluginName);
+    }
+
+    public interface IPlugin
+    {
+        string PluginName { get; }
+        string Version { get; }
+        System.Drawing.Image Image { get; }
+        string Description { get; }
+        int Run(int input1, int input2);
+    }
+
+    public abstract class Plugin: IPlugin
+    {
+        protected Plugin() { }
+
+        public string PluginName => throw new NotImplementedException();
+
+        public string Version => throw new NotImplementedException();
+
+        public Image Image => throw new NotImplementedException();
+
+        public string Description => throw new NotImplementedException();
+
+        public int Run(int input1, int input2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+}

7f26ea3cb8e5d0624a192615170ea606a0eb6997
Savr123 <Sangievv@mail.ru>
2024-04-01 21:14:30 +0300

    Добавил для примера кастом плагин, не знаю зачем, но пусть будет

---
+++ CustomPlugin/CustomPlugin.cs
@@ -0,0 +1,38 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace CustomPlugin
+{
+    [Serializable]
+    public class CustomPlugin : PluginLib.IPlugin
+    {
+        public CustomPlugin()
+        {
+            _pluginName = "CustomPlugin";
+        }
+
+        private string _pluginName;
+        public string PluginName
+        {
+            get
+            {
+                return _pluginName;
+            }
+            set
+            {
+                _pluginName = value;
+            }
+        }
+
+        public string Version { get; set; }
+
+        public Image Image => new Bitmap(80, 20);
+
+        public string Description { get; set; }
+
+        public int Run(int input1, int input2)
+        {
+            return (input1+input2)/2;
+        }
+    }
+}
